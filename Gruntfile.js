59b3a4a3d3057854652028d7bc92c61b87973ead
Gabe Martin <gabemartin@gmail.com>
2014-04-11 17:57:17 -0500

    added "grunt mixins"
    This will pull the latest mixin files from the master repo. It will not
    overwrite your other files, just the files inside of
    /assets/sass/parts/_mixins

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 module.exports = function(grunt) {
   require('load-grunt-config')(grunt);
-  grunt.registerTask('prod', ['sass', 'autoprefixer', 'modernizr', 'concat', 'uglify', 'haml', 'cssmin', 'imagemin', 'htmlmin', 'modernizr']);
+  grunt.registerTask('prod', ['concat', 'sass', 'autoprefixer', 'modernizr', 'uglify', 'haml', 'cssmin', 'imagemin', 'htmlmin', 'modernizr']);
   grunt.registerTask('default', ['connect', 'watch']);
   grunt.registerTask('grunt', ['watch']);
-  grunt.registerTask('setup9876gkjg', ['bowercopy']); // WARNING: Running this task will wipe out your site.
+  grunt.registerTask('mixins', ['bowercopy:mixins', 'concat', 'sass', 'autoprefixer']); // This will update your mixins folder with latest repo.
+  grunt.registerTask('setup9876gkjg', ['bowercopy:boilerplate']); // WARNING: Running this task will wipe out your site.
 };
--- grunt/bowercopy.js
+++ grunt/bowercopy.js
@@ -7,4 +7,12 @@ module.exports = {
           'assets/sass': 'sass-structure'
         }
     },
+    mixins: {
+        options: {
+            clean: true
+        },
+        files: {
+          'assets/sass/parts/_mixins': 'sass-structure/parts/_mixins'
+        }
+    },
 };
